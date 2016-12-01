fc18c9e2c7fded46e0b2770499ef5e5a52f1832b
mdshihabuddin <shihab.uddin@widespace.com>
2016-12-01 23:17:38 +0600

    problem 292 accepted. easy with one line

---
+++ leetcode-problem-solution/src/com/su/leetCode/easy/Problem292NimGame.java
@@ -0,0 +1,13 @@
+package com.su.leetCode.easy;
+
+public class Problem292NimGame {
+
+	public static void main(String[] args) {
+
+	}
+	
+	public static boolean canWinNim(int n) {
+        return !(n%4==0);
+    }
+
+}
