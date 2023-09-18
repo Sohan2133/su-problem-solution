9ff280ab9a814e465a58cac00255e3b6f7473500
sohanur-rahman <sohan.just.cse19@gmail.com>
2023-09-18 17:08:02 +0600

    Solved for product table

--- leetcode/database/easy/1777-product-price.sql
+++ leetcode/database/easy/1777-product-price.sql
@@ -6,7 +6,10 @@ Products
 | store       | enum    |
 | price       | int     |
 +-------------+---------+
-
+----(product_id,store) is the primary key for this table.
+--store is an ENUM of type ('store1', 'store2', 'store3') where each represents the store this product is available at.
+---price is the price of the product at this store.
+--Write an SQL query to find the price of each product in each store.
 DROP TABLE IF EXISTS Products;
 CREATE TABLE IF NOT EXISTS Products (
 	product_id int,
@@ -29,3 +32,13 @@ select
 	sum(case when store = 'store3' then price else 0 end) as store3
 from Products
 group by product_id
+
+---Sohan's Solution---
+
+select
+product_id,
+sum(case when store = 'store1' then price else null end) as store1,
+sum(case when store = 'store2' then price else null end) as store2,
+sum(case when store = 'store3' then price else null end) as store3
+from `query_solving2144.product_price`
+group by product_id;
--- leetcode/database/easy/1795-rearrange-product.sql
+++ leetcode/database/easy/1795-rearrange-product.sql
@@ -6,6 +6,8 @@
 | store2      | int     |
 | store3      | int     |
 +-------------+---------+
+----Write a solution to rearrange the Products table so that each row has (product_id, store, price). 
+--If a product is not available in a store, do not include a row with that product_id and store combination in the result table.
 
 DROP TABLE IF EXISTS Products;
 CREATE TABLE IF NOT EXISTS Products (
@@ -36,4 +38,12 @@ union
 select product_id, 'store1' as store , store3 as price from Products where store3 is not null
 order by product_id
 ;
-	
+
+----Sohan's Solution----
+
+select product_id, 'store1' as store, store1 as price from Products where store1 is not null
+union
+select  product_id, 'store2' as store, store2 as price  from Products where store2 is not null  
+union
+select product_id, 'store3' as store, store3 as price from Products where store3 is not null
+order by product_id asc;    
